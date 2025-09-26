015103e405ad01ec393adb96c427c6a055ae85d7
Jaeho Cho <jaeho2025@gmail.com>
2025-09-26 05:25:19 -0400

    vid2

--- train.py
+++ train.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.pt")
 
 # Train the model on your custom dataset
-model.train(data="/home/jaeho/datasets/vid_1/data.yaml", epochs=100, imgsz=640)
+model.train(data="/home/jaeho/datasets/combined/data.yaml", epochs=100, imgsz=640)
